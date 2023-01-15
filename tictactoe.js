7e2ae68e3a0266e7c824f41d01a392f1c0f4edd2
Serapis <83037354+Serapis342@users.noreply.github.com>
2023-01-15 21:12:12 +0100

    Add files via upload

---
+++ tictactoe.js
@@ -0,0 +1,87 @@
+window.onload = function() {
+    window.player = 1;
+    window.text = document.getElementById('show-text');
+    window.noWinner = true;
+    window.countXs = 0;
+    window.countOs = 0;
+
+    window.one = document.getElementById('0');
+    window.two = document.getElementById('1');
+    window.three = document.getElementById('2');
+    window.four = document.getElementById('3');
+    window.five = document.getElementById('4');
+    window.six = document.getElementById('5');
+    window.seven = document.getElementById('6');
+    window.eight = document.getElementById('7');
+    window.nine = document.getElementById('8');
+    
+    window.table = [one, two, three, four, five, six, seven, eight, nine];
+    window.gameField = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+    table.forEach(element => {
+        element.addEventListener('click', function () {
+            if(noWinner == true) {
+                if (element.innerHTML == '') {
+                    element.innerHTML = whichPlayer();
+                    gameField[element.id] = element.innerHTML;
+                }
+            }
+        });
+    });
+}
+
+function whichPlayer() {
+    if (player == 1) {
+        player = 2;
+        countXs++;
+        return 'X';
+    } else {
+        player = 1;
+        countOs++;
+        return 'O';
+    }
+}
+
+function win(winner) 
+{
+    if (!noWinner) {
+        location.reload();
+    }
+
+    noWinner = false;
+    if(winner != 'Draw') 
+    {
+        text.innerHTML = `Player ${winner} has won!`;
+    } else {
+        text.innerHTML = `It's a draw!`;
+    }
+}
+
+document.onclick = function() 
+{
+    let playerChar = player == 1 ? 'X' : 'O';
+    text.innerHTML = `It's ${playerChar}'s turn!`;
+
+    if(gameField[0] == gameField[1] && gameField[1] == gameField[2]) {
+        win(table[0].innerHTML);
+    } else if(gameField[3] == gameField[4] && gameField[4] == gameField[5]) {
+        win(table[3].innerHTML);
+    } else if(gameField[6] == gameField[7] && gameField[7] == gameField[8]) {
+        win(table[6].innerHTML);
+    } else if(gameField[0] == gameField[3] && gameField[3] == gameField[6]) {
+        win(table[0].innerHTML);
+    } else if(gameField[1] == gameField[4] && gameField[4] == gameField[7]) {
+        win(table[1].innerHTML);
+    }   else if(gameField[2] == gameField[5] && gameField[5] == gameField[8]) {
+        win(table[2].innerHTML);
+    } else if(gameField[0] == gameField[4] && gameField[4] == gameField[8]) {
+        win(table[0].innerHTML);
+    } else if(gameField[2] == gameField[4] && gameField[4] == gameField[6]) {
+        win(table[2].innerHTML);
+    } else if(countXs == 5 && countOs == 4) {
+        win('Draw');
+        console.log('Draw');
+    } else {
+        return;
+    }
+}
